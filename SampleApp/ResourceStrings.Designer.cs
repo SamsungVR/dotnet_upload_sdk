0f43edb7edf800307a6e194871937e20b25ca564
vnarasimhan <narasimhan@samsung.com>
2017-02-17 18:11:59 -0600

    Add resources needed for latest changes

--- SampleApp/ResourceStrings.Designer.cs
+++ SampleApp/ResourceStrings.Designer.cs
@@ -160,7 +160,7 @@ namespace SampleApp {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {&quot;configs&quot;:[{&quot;url&quot;: &quot;https://samsungvr.com/api&quot;, &quot;apiKey&quot;: &quot;58518419e38ac0000cb5fba8.a7cNtpUHZxAcSLa8x_wc-mOsuWED-4IcNroyJKaLP8U&quot;}]}.
+        ///   Looks up a localized string similar to .
         /// </summary>
         internal static string preConfiguredEP {
             get {
